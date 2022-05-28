cbeddd524d4d9a813d4f7344f88ad79169de5b78
luckythota <97425859+luckythota@users.noreply.github.com>
2022-05-28 20:42:14 +0530

    n=int(input()) for i in range(n):     s=input()     flag=0     for i in s:         if i in '0123456789':             flag=1     if(flag==1):         print('Yes')     else:         print('No')

---
+++ string_contains_digits_or_not.py
@@ -0,0 +1,11 @@
+n=int(input())
+for i in range(n):
+    s=input()
+    flag=0
+    for i in s:
+        if i in '0123456789':
+            flag=1
+    if(flag==1):
+        print('Yes')
+    else:
+        print('No')
