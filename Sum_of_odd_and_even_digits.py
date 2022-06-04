17442ec2d4124faaec7cea529ad9ece57e67491f
luckythota <97425859+luckythota@users.noreply.github.com>
2022-06-04 20:49:16 +0530

    n=input() a=list(map(int,input().split())) sum=0 sum1=0 for i in range(len(a)):     if(i%2!=0):         sum=sum+a[i]     if(i%2==0):         sum1=sum1+a[i] if(sum-sum1==0):     print('YES') else:     print('NO')

---
+++ Sum_of_odd_and_even_digits.py
@@ -0,0 +1,13 @@
+n=input()
+a=list(map(int,input().split()))
+sum=0
+sum1=0
+for i in range(len(a)):
+    if(i%2!=0):
+        sum=sum+a[i]
+    if(i%2==0):
+        sum1=sum1+a[i]
+if(sum-sum1==0):
+    print('YES')
+else:
+    print('NO')
