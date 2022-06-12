5bc3caa0ee4be1984aa0072f4e6a8872a7180744
luckythota <97425859+luckythota@users.noreply.github.com>
2022-06-12 19:39:48 +0530

    n=int(input()) for i in range(65,n+65):     for j in range(1,n+1):         print(chr(i),end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,5 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n-1):
-        print(j,end='')
-    for j in range(n-3,0,-1):
-        print(j,end='')
+for i in range(65,n+65):
+    for j in range(1,n+1):
+        print(chr(i),end=' ')
     print()
