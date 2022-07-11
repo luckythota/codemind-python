3181b1c0c711bfe4666205624398a409deb0de6a
luckythota <97425859+luckythota@users.noreply.github.com>
2022-07-11 18:00:58 +0530

    n=int(input()) a=list(map(int,input().split())) count=0 sum=0 for i in range(n):     count=0     m=a[i]     for j in range(1,m+1):         if a[i]%j==0:             count=count+1     if count==2:         sum=sum+1 print(sum)

---
+++ Primes_in_the_array.py
@@ -0,0 +1,13 @@
+n=int(input())
+a=list(map(int,input().split()))
+count=0
+sum=0
+for i in range(n):
+    count=0
+    m=a[i]
+    for j in range(1,m+1):
+        if a[i]%j==0:
+            count=count+1
+    if count==2:
+        sum=sum+1
+print(sum)
