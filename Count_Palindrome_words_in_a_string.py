01273fa52702a938b3e97ef8b92967cd2a4a490d
luckythota <97425859+luckythota@users.noreply.github.com>
2022-09-04 16:17:23 +0530

    def checkpalin(word):     if word.lower()==word.lower()[::-1]:         return True def countpalin(word):     count=0     v=str.split(" ")     for elements in v:         if checkpalin(elements):             count+=1     print(count) str=input() countpalin(str)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,12 +1,13 @@
 def checkpalin(word):
     if word.lower()==word.lower()[::-1]:
         return True
-def countpalin(str):
+def countpalin(word):
     count=0
     v=str.split(" ")
     for elements in v:
         if checkpalin(elements):
             count+=1
-    print(count) 
+    print(count)
 str=input()
 countpalin(str)
+    
