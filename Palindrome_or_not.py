3acc48145f2a37b0fce39738a6556fcdd78e06e0
luckythota <97425859+luckythota@users.noreply.github.com>
2022-09-04 16:10:53 +0530

    s=input() flag=0 s=s.lower() for i in range(0,len(s)//2):     if s[i]!=s[len(s)-i-1]:         flag=1         break if flag==0:     print('True') else:     print('False')

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -2,11 +2,10 @@ s=input()
 flag=0
 s=s.lower()
 for i in range(0,len(s)//2):
-    if(s[i]!=s[len(s)-i-1]):
+    if s[i]!=s[len(s)-i-1]:
         flag=1
         break
-if(flag==0):
+if flag==0:
     print('True')
 else:
     print('False')
-    
