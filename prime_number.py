b4ea7323ea56f4122ee93ef6e6931b0de5ec637e
luckythota <97425859+luckythota@users.noreply.github.com>
2022-09-08 20:04:51 +0530

    n=int(input()) flag=0 for i in range(2,n):     if n%i==0:         flag=1 if flag==0:     print('prime') else:     print('not a prime')

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,9 @@
 n=int(input())
-i=2
-count=0
-for i in range(i,n,1):
-    if(n%i==0):
-        count=count+1
-if(count==0):
+flag=0
+for i in range(2,n):
+    if n%i==0:
+        flag=1
+if flag==0:
     print('prime')
 else:
     print('not a prime')
