4892a4b87b7fd054aa74c85c68c5315b28ace85d
luckythota <97425859+luckythota@users.noreply.github.com>
2022-09-13 10:35:34 +0530

    m,n=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) l=[] m=[] for i in a:     if i in b:         l.append(i) for i in l:     if i not in m:         m.append(i)     else:         flag=1 print(len(m))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -2,11 +2,13 @@ m,n=map(int,input().split())
 a=list(map(int,input().split()))
 b=list(map(int,input().split()))
 l=[]
+m=[]
 for i in a:
     if i in b:
         l.append(i)
-for i in b:
-    if i in a:
-        l.append(i)
-v=set(l)
-print(len(v))
+for i in l:
+    if i not in m:
+        m.append(i)
+    else:
+        flag=1
+print(len(m))
